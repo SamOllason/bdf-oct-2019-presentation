f91f4abfb0e1ebcf87c83535490bb5e1eade48ba
Samuel Ollason <samuelollason@Samuels-MacBook-Pro-2.local>
2019-11-21 10:45:26 +0000

    Updates

---
+++ precache-manifest.8b9946e58b82b6aa529e1ab0efc5d207.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9d4bff94ebc58eb75827f0336daeae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b94896d1bcba4d0af855",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5070435bbbf8aa577d42",
+    "url": "/static/js/2.0f036af0.chunk.js"
+  },
+  {
+    "revision": "b94896d1bcba4d0af855",
+    "url": "/static/js/main.b3db9005.chunk.js"
+  },
+  {
+    "revision": "f649b18296e62814dce0",
+    "url": "/static/js/runtime-main.2c7d9cb5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
