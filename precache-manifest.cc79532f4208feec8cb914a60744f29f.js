572b8a278f166409b7ad89d8230cb766f5820ab6
Samuel Ollason <samuelollason@Samuels-MacBook-Pro-2.local>
2019-11-21 10:46:59 +0000

    Updates

---
+++ precache-manifest.cc79532f4208feec8cb914a60744f29f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e6603a5b72390fc584a23f1ba293e5",
+    "url": "/bdf-oct-2019-presentation/index.html"
+  },
+  {
+    "revision": "b479de14b3d5195ca08c",
+    "url": "/bdf-oct-2019-presentation/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5070435bbbf8aa577d42",
+    "url": "/bdf-oct-2019-presentation/static/js/2.0f036af0.chunk.js"
+  },
+  {
+    "revision": "b479de14b3d5195ca08c",
+    "url": "/bdf-oct-2019-presentation/static/js/main.2b3836ac.chunk.js"
+  },
+  {
+    "revision": "21f64db9778b0a9877c2",
+    "url": "/bdf-oct-2019-presentation/static/js/runtime-main.1a504ed5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/bdf-oct-2019-presentation/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b9946e58b82b6aa529e1ab0efc5d207.js"
+  "/bdf-oct-2019-presentation/precache-manifest.cc79532f4208feec8cb914a60744f29f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bdf-oct-2019-presentation/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
